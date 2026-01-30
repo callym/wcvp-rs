f6cc37882c2270d6e791eab83a2434bc48978ba5
callym <hi@callym.com>
2026-01-30 16:16:14 +0000

    Remove unneeded cargo features

--- src/lib.rs
+++ src/lib.rs
@@ -5,22 +5,3 @@ mod wcvp;
 
 pub use error::Error;
 pub use wcvp::{Climate, Data, HybridType, Metadata, Statistics, Status, TaxonRank, Version, Wcvp};
-
-#[tokio::test]
-async fn test() -> Result<(), Box<dyn std::error::Error>> {
-  use std::path::PathBuf;
-
-  let mut dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-  dir.push("data/wcvp.zip");
-  dbg!(&dir);
-
-  let mut data = Wcvp::from_file(dir).await?;
-
-  dbg!(data.metadata().unwrap());
-
-  data.statistics()?;
-
-  dbg!(data.from_powo("969084-1"));
-
-  Ok(())
-}
